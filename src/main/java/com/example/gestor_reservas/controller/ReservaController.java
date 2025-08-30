362c342a3525d0a03655fa4264b8eefb568b178d
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2025-08-30 13:08:55 -0300

    add: permisos para consumirlo desde mi frontendd

--- src/main/java/com/example/gestor_reservas/controller/ReservaController.java
+++ src/main/java/com/example/gestor_reservas/controller/ReservaController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/reservas")
 public class ReservaController {
